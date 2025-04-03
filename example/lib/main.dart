83087db6d317ac1efd73274cb81dabc70a56087f
sumitsharansatsangi <sharansumitkumar@gmail.com>
2025-04-03 20:34:55 +0530

    code updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:intl/intl.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
